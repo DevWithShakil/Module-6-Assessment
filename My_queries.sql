70e2a8b0426931458c234cacab90b6f321d4d435
DevWithShakil <m.shakilkhan720@gmail.com>
2025-08-09 22:16:55 +0600

    final checking done

--- My_queries.sql
+++ My_queries.sql
@@ -7,7 +7,6 @@ ORDER BY salary ASC
 OFFSET 2 LIMIT 1;
 
 
--------------------------------------------------------
 -- Q2: Find 2nd lowest salary using subquery
 
 SELECT MIN(salary) AS second_lowest_salary
@@ -16,6 +15,7 @@ WHERE salary > (SELECT MIN(salary) FROM employee);
 
 
 -- Q3: Average pending balance (active accounts only, < 4300)
+
 SELECT open_branch_id,
        AVG(pending_balance) AS avg_pending_balance
 FROM account
@@ -25,6 +25,7 @@ HAVING AVG(pending_balance) < 4300;
 
 
 -- Q4: Pagination query (Page 2, 3 rows per page)
+
 SELECT *
 FROM employee
 ORDER BY emp_id
